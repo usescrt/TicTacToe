41de94c60be07e89a167628617bd750cf36ab5c2
ms <muratsahin01@gmail.com>
2023-02-19 02:50:08 +0300

    add validation and loading animate

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,64 +2,98 @@ import React, { useState, useEffect } from "react";
 import { signInAnonymously, getAuth, onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../../firebaseConfig";
 import { setDoc, doc } from "firebase/firestore";
-import { Appbar, List, TextInput, Button } from "react-native-paper";
+import {
+  Appbar,
+  TextInput,
+  Button,
+  Dialog,
+  Portal,
+  Provider,
+  Text,
+  ActivityIndicator,
+  HelperText,
+} from "react-native-paper";
 
 function HomeScreen({ navigation }) {
   // Set TextInput Data
-  const [textInput, setTextInput] = useState("");
 
-  // Set Firebase UserUID
+  var [textInput, setTextInput] = useState("");
 
-  const [userUID, setUserUID] = useState("");
+  // ...
 
-  // User sign in Anonymously
+  var [visible, setVisible] = React.useState(true);
 
-  function signIn() {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in.
-        console.log("Sing in");
-        navigation.navigate("GameList");
-      } else {
-        // No user is signed in.
-        signInAnonymously(auth)
-          .then((result) => {
-            console.log("Sing In");
-
-            setUserUID(result.user.uid);
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ...
-            console.log(errorCode);
-            console.log(errorMessage);
-          });
-      }
-    });
-  }
-
-  // Post Username from Firestore
-
-  async function postUsernameFirestore() {
-    try {
-      const docRef = await setDoc(doc(db, "Users", userUID), {
-        username: textInput,
-        userUID: userUID,
-      }).then(() => {
-        // Open GameList Screen
-        navigation.navigate("GameList");
-      });
-      // console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
+  // init page
 
   useEffect(() => {
     // ...
-    signIn();
+    checkSignIn();
   }, []);
+
+  // User sign in Anonymously
+
+  const checkSignIn = () => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in.
+        console.log("Sign in");
+        // Open Game List Screen
+        openGameListScreen();
+      } else {
+        // No user is signed in.
+        setVisible(false);
+      }
+    });
+  };
+
+  const signIn = () => {
+    signInAnonymously(auth).then((result) => {
+      console.log("Sign In");
+
+      postUsernameFirestore(result.user.uid);
+    });
+  };
+
+  // Post Username from Firestore
+
+  const postUsernameFirestore = async (userUID) => {
+    const docRef = await setDoc(doc(db, "Users", userUID), {
+      username: textInput,
+      userUID: userUID,
+    }).then(() => {
+      setVisible(false);
+      openGameListScreen();
+    });
+  };
+
+  const handleDoneButton = () => {
+    if (textInput.length > 2) {
+      setVisible(true);
+      signIn();
+    }
+  };
+
+  const hasErrorsTextInput = () => {
+    return textInput.length < 3;
+  };
+
+  const openGameListScreen = () => {
+    // Open GameList Screen
+    navigation.navigate("GameList");
+  };
+
+  if (visible)
+    return (
+      <Provider visible={true}>
+        <Portal>
+          <Dialog visible={visible}>
+            <Dialog.Content>
+              <ActivityIndicator size={"large"} />
+            </Dialog.Content>
+          </Dialog>
+        </Portal>
+      </Provider>
+    );
 
   return (
     <>
@@ -68,7 +102,7 @@ function HomeScreen({ navigation }) {
         <Appbar.Action
           icon="check"
           onPress={() => {
-            postUsernameFirestore();
+            handleDoneButton();
           }}
         />
       </Appbar.Header>
@@ -80,6 +114,9 @@ function HomeScreen({ navigation }) {
         }}
         style={{ margin: 16 }}
       />
+      <HelperText type="error" visible={hasErrorsTextInput()}>
+        Must be min. 3 char!
+      </HelperText>
     </>
   );
 }
